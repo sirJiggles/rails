15a1c44dbf8ed8ab2d078b15fd1f807ba9af3ab7
garethFuller <gareth-fuller@hotmail.co.uk>
2015-09-27 19:27:33 +0200

    Remove demo controller, starting on app in book

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'say/hello'
-
-  get 'say/goodbye'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
