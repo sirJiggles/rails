1e4b072a28f46745e9ba8725ad8cd7b9e4fc0c6a
garethFuller <gareth-fuller@hotmail.co.uk>
2015-09-28 07:12:32 +0200

    More testing

--- demo/test/models/product_test.rb
+++ demo/test/models/product_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "Properties required" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "Price must be numeric + positive" do
+    product = Product.new(
+      title: 'Test product one',
+      description: 'some descr',
+      image_url: 'someImageUrl'
+    )
+    errMsg = 'must be greater than or equal to 0.01'
+    product.price = -1
+    assert product.invalid?
+    assert_equal [errMsg], product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal [errMsg], product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "Should not allow a duplicate product title" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: 'This is the second descrption',
+      image_url: 'http://someUrl.com',
+      price: 20.00
+    )
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
 end
