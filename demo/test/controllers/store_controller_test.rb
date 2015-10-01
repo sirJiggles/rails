069c3b5d631a5cfc68da9ec95f3ac09cb1518ce4
Gareth Fuller <garethfuller@Gareths-MacBook-Pro.local>
2015-10-01 18:54:53 +0200

    Added some tests and the price on the store list

--- demo/test/controllers/store_controller_test.rb
+++ demo/test/controllers/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.m-site-navigation li a', minimum: 4
+    assert_select '.m-store-list-item', 3
+    assert_select '.m-store-list-item-price', /\$[,\d]+\.\d\d/
   end
 
 end
